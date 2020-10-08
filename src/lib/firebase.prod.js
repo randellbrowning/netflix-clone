8bfc90b913dcc975a9d3e2571173a03470571963
Randell Browning <rbrowning@SmartCommunications.com>
2020-10-08 10:27:02 -0400

    initial import

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,29 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import {seedDatabase} from '../seed';
+
+// 1) when seeding the database you'll have to uncomment this!
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+
+};
+
+const firebase = Firebase.initializeApp(config);
+
+//seedDatabase(firebase);
+
+// 2) when seeding the database you'll have to uncomment this!
+// seedDatabase(firebase);
+// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
+
+export { firebase };
